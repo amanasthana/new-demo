318aa181dba731958673981f266be1627f8ae38e
asthanaa <amanasthana7@gmail.com>
2023-11-20 23:16:27 +0530

    asd1

--- newdemo11/urls.py
+++ newdemo11/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for newdemo11 project.
+URL configuration for newdemo1111 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- newdemo11/wsgi.py
+++ newdemo11/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for newdemo11 project.
+WSGI config for newdemo1111 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newdemo11.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newdemo1111.settings')
 
 application = get_wsgi_application()
